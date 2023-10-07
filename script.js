9619f0d70f8f465609ae93b1e0d9041c1c5326df
Elhour <118532248+Elhour-YT@users.noreply.github.com>
2023-10-07 22:12:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const searchForm = document.querySelector('.search');
-    const movies = document.querySelectorAll('.main > div');
-    const noResultsMessage = document.querySelector('.no-results');
 
-    searchForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const searchTerm = event.target.q.value.toLowerCase();
-        let hasResults = false;
-
-        movies.forEach(movie => {
-            const title = movie.querySelector('h2').textContent.toLowerCase();
-            if (title.includes(searchTerm)) {
-                movie.style.display = 'block';
-                hasResults = true;
+    document.querySelector('.search-button').addEventListener('click', e =>  {
+        e.preventDefault();
+        document.querySelectorAll('h2').forEach(title => {
+ 
+            if (title.textContent.toLowerCase().includes(e.currentTarget.closest(".search").querySelector('.search-bar').value.toLowerCase())) {
+                          console.log('dnbfdnbdfnf ndf');
+                title.parentElement.style.display = 'block';
             } else {
-                movie.style.display = 'none';
+                title.parentElement.style.display = 'none';
             }
         });
 
-        if (hasResults) {
-            noResultsMessage.style.display = 'none';
-        } else {
-            noResultsMessage.style.display = 'block';
-        }
     });
 });
