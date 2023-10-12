17e72a89b4c8a978a0c1a1764d4be5f0396bfc15
Elhour <118532248+Elhour-YT@users.noreply.github.com>
2023-10-12 18:33:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,164 @@
+let movies = {
+
+    'AFTER : EVERYTHING' : {
+                        'sous-title'   : ' Actors : Josephine Langford, Mimi Keene, Hero Fiennes-Tiffin, Benjamin Mascolo, Arielle Kebbel, Jessica Webber',
+                        'description'  : ' Author Hardin Scott travels to Portugal to try to make Natalie forgive what he did to her.',
+                        'img'          : 'https://spgeng.rosselcdn.net/sites/default/files/dpistyles_v2/ena_16_9_extra_big/2023/09/05/node_706689/55026969/public/2023/09/05/B9735023772Z.1_20230905114415_000%2BGTNND5AM3.1-0.jpg?itok=pYwEEXXp1693907061',
+                        'link'         : 'https://tinyzonetv.se/watch-movie/watch-the-equalizer-3-2023-free-99865.9893992',
+                    },
+    'THE NUN II' : {
+                        'sous-title'   : ' Actors : Taissa Farmiga, Bonnie Aarons, Anna Popplewell, Storm Reid, Jonas Bloquet, Vera Farmiga',
+                        'description'  : ' In France, in 1956, a priest was murdered in a boarding school. After the priests death, things happen that can no longer be explained rationally. Sister Irene is once again confronted with demonic power.',
+                        'img'          : 'https://movies.sterkinekor.co.za/CDN/media/entity/get/FilmTitleGraphic/HO00002810?referenceScheme=HeadOffice&allowPlaceHolder=true',
+                        'link'         : 'https://vido.lol/embed-srpcksmvajxb.html?',
+                    },
+    'EQUALIZER 3' : {
+                        'sous-title'   : 'Denzel Washington, Dakota Fanning, Sonia Ben Ammar, Gaia Scodellaro, David Denman, Andrea Scarduzio',
+                        'description'  : ' Since giving up his life as a government assassin, Robert McCall has struggled to make peace with his past demons and finds strange comfort in defending the oppressed. While he has found his haven of peace in the south of Italy, he discovers that his friends are under the control of the local mafia. When events take a deadly turn, McCall knows what he must do: protect his friends by taking on the mafia.',
+                        'img'          : 'https://quefairedesmomes.fr/wp-content/uploads/2023/09/the-equalizer-3.jpg',
+                        'link'         : 'https://tinyzonetv.se/watch-movie/watch-the-equalizer-3-2023-free-99865.9893992',
+                    },
+    'GRAN TURISMO' : {
+                        'sous-title'   : ' Actors : Archie Madekwe, Orlando Bloom, David Harbour, geri Halliwell, Daren Barnet, Djimon Hounsou',
+                        'description'  : '  Young Jann Mardenborough is dying to be a racing driver, but doesnt have the financial means to be part of the elite sport. Thats why he only stepped on the accelerator in the video game Gran Turismo and achieved enormous success.',
+                        'img'          : 'https://www.gran-turismo.com/images/c/i13ynTpBw8HAmb.jpg',
+                        'link'         : 'https://vido.lol/embed-g6vomwnavths.html?',
+                    },
+    'BLUE BEETLE' : {
+                        'sous-title'   : ' Actors : Bruno Marquezine, Xolo Mariduena, Becky G, George Lopez, Susan Sarandon, Belissa Escobedo',
+                        'description'  : '  Jaime Reyes is a young man full of ambition. As he returns from college, the Scarab, an ancient relic of extraterrestrial biotechnology, chooses to make him its host. The latter then sees himself dressed in extraordinary armor which grants him extraordinary and unpredictable powers. Everything then changes for Jaime who becomes the superhero Blue Beetle. ',
+                        'img'          : 'https://www.pixground.com/wp-content/uploads/2023/06/Blue-Beetle-4K-Wallpaper-jpg.webp',
+                        'link'         : 'https://vido.lol/embed-7lmj09bhqsfm.html?',
+                    },
+    'AGENT STONE' : {
+                        'sous-title'   : ' Actors : Alia Bhatt, Gal Gadott, Jamie Dornan, Jing Lusi, Sohpie Okonedo, Glenn Close',
+                        'description'  : '  Intelligence agent Rachel Stone is the only woman who stands between her powerful global peacekeeping organization and the loss of her most valuable and dangerous asset. ',
+                        'img'          : 'https://i0.wp.com/filmonizirani.net/wp-content/uploads/2022/09/Heart-of-Stone-1.jpg?fit=800%2C450&ssl=1',
+                        'link'         : 'https://vido.lol/embed-t5qlu7v4uw8d.html?',
+                    },
+    'BARBIE' : {
+                        'sous-title'   : ' Actors : Margot Robbie, Ryan Gosling, Will Ferrell, Emma Mackey, Simu Liu, Micheal Cera',
+                        'description'  : '  Barbie, who lives in Barbie Land, is kicked out of the country for being less than a perfect-looking doll; with nowhere to go, she leaves for the human world and seeks true happiness.',
+                        'img'          : 'https://www.student.si/wp-content/uploads/2023/07/Ocena-filma-Barbie.jpeg',
+                        'link'         : 'https://vido.lol/embed-8r6nipku5pt8.html?',
+                    },
+    'MISSION IMPOSSIBLE 7 : DEAD RECKONING, PART 1' : {
+                        'sous-title'   : ' Actors : Hayley Atwell, Tom Cruise, Rebecca Ferguson, Vanessa Kirby, Essai Morales, Simon Pegg',
+                        'description'  : ' Ethan Hunt and the IMF team must track down a terrifying new weapon that threatens all of humanity if it falls into the wrong hands. With control of the future and the fate of the world at stake, a deadly race around the globe begins. ',
+                        'img'          : 'https://www.screentune.com/wp-content/uploads/2023/07/Critique_-Mission-Impossible-Dead-Reckoning-partie-1-_2023-_ScreenTune.jpg',
+                        'link'         : 'https://vido.lol/embed-xpjngi1nx2nr.html?',
+                    },
+    'INDIANA JONES AND THE DIAL OF DESTINY' : {
+                        'sous-title'   : ' Actors : Harrison Ford, Mads Mikkelsen, Phoebe Waller-Bridge, Antonio Banderas, Boyd Holbrook, Thomas Kretschmann',
+                        'description'  : '  Indiana Jones receives a surprise visit from his goddaughter Helena Shaw, who is looking for a rare artifact that her father entrusted to Indiana in the past: the famous Archimedes dial, a relic said to have the power to locate cracks temporal. Helena steals the object and rushes out of the country to sell it to the highest bidder. Indy has no choice but to give chase. ',
+                        'img'          : 'https://movies.monnaie-services.com/FR/backdrop/25/800/VKI33.jpg',
+                        'link'         : 'https://vido.lol/embed-wkpkkn9239un.html?',
+                    },
+    'NO HARD FEELINGS' : {
+                        'sous-title'   : ' Actors : Jennifer Lawrence, Andrew Barth Feldman, Laura Benanti, Nathalie Morales, Matthew Broderick, Hasan Minhaj',
+                        'description'  : '  Maddie is on the verge of losing her childhood home and thinks shes found the solution to her financial troubles: wealthy parents are looking for someone to date their introverted 19-year-old son Percy to unseat him before he dont go to college. Much to Maddies surprise, Percy makes this challenge harder than expected and time is running out. She has one summer to take up this challenge or find herself homeless. ',
+                        'img'          : 'https://image.tmdb.org/t/p/w1280/re6SXqEOBczX3iG9x3PmNSbiLtT.jpg',
+                        'link'         : 'https://vido.lol/embed-g5ggx9qwrbsi.html?',
+                    },
+    'THE FLASH' : {
+                        'sous-title'   : ' Actors : Ezra Miller, Micheal Keaton, Ben Affleck, Sasha Calle, Kiersey Clemons, Michael Shanon',
+                        'description'  : '  Flash uses his time-travelling superpowers to change events from the past. However, when his attempt to save his family inadvertently alters the future, he finds himself trapped in a reality in which General Zod has returned, threatening annihilation. ',
+                        'img'          : 'https://ntvb.tmsimg.com/assets/p12742479_v_h8_ad.jpg?w=960&h=540',
+                        'link'         : 'https://vido.lol/embed-m3fphcsi6rrx.html?',
+                    },
+    'DUNGEONS & DRAGONS : HONOR THIEVES' : {
+                        'sous-title'   : ' Actors : Sophia Lilis, Chris Pine, Michelle Rodriguez, Daisy Head, Justice Smith, Chloe Coleman',
+                        'description'  : '  A charming thief and a band of unlikely adventurers embark on an epic quest to recover a lost relic, but things go dangerously wrong when they run into the wrong people. ',
+                        'img'          : 'https://www.geekgeneration.fr/wp-content/uploads/2022/12/Donjons-Dragons-LHonneur-des-voleurs.jpg',
+                        'link'         : 'https://vido.lol/embed-v9qkjnfkxzh6.html?',
+                    },
+    'SUPER MARIO BROS : THE MOVIE' : {
+                        'sous-title'   : ' Actors : Anya Taylor Joy, Chris Pratt, Jack Black, Charlie Day, Seth Rogen, Charles Martinet',
+                        'description'  : '  A plumber named Mario navigates an underground maze with his brother, Luigi, trying to rescue a captured princess. Film adaptation of the famous video game. ',
+                        'img'          : 'https://images.pathe-thuis.nl/33019_1920x1080.jpg',
+                        'link'         : 'https://vido.lol/embed-rhqhyjv5hrht.html?',
+                    },
+    'GAURDIANS OF THE GALAXY : VOLUME 3' : {
+                        'sous-title'   : ' Actors : Chris Pratt, Zoe Saldana, Will Poulter, Sean Gunn, Karen Gillan, Vin Diesel',
+                        'description'  : '  The Guardians of the Galaxy will have to fight again Ayesha, the priestess of the Sovereigns, face the creator of Rocket who wants to recover him at all costs and challenge Adam Warlock ready to do anything to kill them. ',
+                        'img'          : 'https://images3.alphacoders.com/131/1310205.jpeg',
+                        'link'         : 'https://vido.lol/embed-tywsdxz06xor.html?',
+                    },
+    'FAST FURIOUS X' : {
+                        'sous-title'   : ' Actors : Vin Diesel, Jason Momoa, Alan Ritchson, Rita Moreno, Michelle Rodriguez, Brie Larson',
+                        'description'  : '  During numerous missions and against all odds, Dom Toretto and his family have outwitted and overtaken all enemies in their path. Now they must face the deadliest opponent they have ever faced. Fueled by vengeance, a terrifying threat emerges from the shadows of the past to shatter Doms world and destroy everything and everyone he loves.',
+                        'img'          : 'https://iforum-de.c.hihonor.com/es_data/images/10000/2023/6/17/feab5cf2-cb01-4486-a3bd-17784b645580.jpg',
+                        'link'         : 'https://vido.lol/embed-ioe0zxqslr1f.html?',
+                    },
+    'SPIDER-MAN : ACROSS THE SPIDER-VERSE' : {
+                        'sous-title'   : ' Actors : Hailee Steinfield, Oscar Isaac, Shameik Moore, Karan Soni, Daniel Kaluuya, Jason Shwartzman',
+                        'description'  : '  After reuniting with Gwen Stacy, Brooklyns friendly full-time Neighborhood Spider-Man is catapulted through the multiverse, where he encounters a team of Spider-People tasked with protecting his very existence. ',
+                        'img'          : 'https://images3.alphacoders.com/131/1318066.jpeg',
+                        'link'         : 'https://vido.lol/embed-e1cdnfpxv3jr.html?',
+                    },
+    'TRANSFORMERS : RISE OF THE BEASTS' : {
+                        'sous-title'   : ' Actors : Anthony Ramos, Liza Koshy, Pete Davidson, Peter Collin, Ron Perlman, Michelle Yeoh',
+                        'description'  : '  The Decepticons just arrived on Earth in 1994, as did Optimus Prime, who has been around for quite a bit longer. Archaeologist Elena and Private Noah in Peru discover traces of an ancient Transformer conflict on Earth. At the time, the Maximals, Predacons, and Terrorcons fought each other, and they came back to life a little later. ',
+                        'img'          : 'https://flxt.tmsimg.com/assets/p20201199_v_h10_am.jpg',
+                        'link'         : 'https://vido.lol/embed-5t2vrbpib5tt.html?',
+                    },
+    'TOP GUN : MAVERICK' : {
+                        'sous-title'   : ' Actors : Tom Cruise, Miles Teller, Val Kilmer, Glen Powell, Jennifer Connely, Monica Barbaro',
+                        'description'  : '  After more than 30 years of service as one of the Navys finest airmen, Pete "Maverick" Mitchell is in his place, pushing the envelope as a brave test pilot and dodging the rank advancement that would ground. Training young graduates for a special mission, Maverick must confront the ghosts of his past and his deepest fears, culminating in a mission that demands the ultimate sacrifice from those who choose to pilot it. ',
+                        'img'          : 'https://ntvb.tmsimg.com/assets/p14325539_v_h8_aj.jpg?w=1280&h=720%22',
+                        'link'         : 'https://vido.lol/embed-n91p0xyllo9r.html?',
+                    },
+    'AVATAR : THE WAY OF WATER' : {
+                        'sous-title'   : ' Actors : Sam Worthington, Zoe Saldana, Sigourney Weaver, Kan Winslet, Stephen Lang, Michelle Yeoh',
+                        'description'  : ' Since giving up his life as a government assassin, Robert McCall has struggled to make peace with his past demons and finds strange comfort in defending the oppressed. While he has found his haven of peace in the south of Italy, he discovers that his friends are under the control of the local mafia. When events take a deadly turn, McCall knows what he must do: protect his friends by taking on the mafia. Jake Sully and Neytiri have formed a family and do everything to stay as close together as possible. However, they are forced to leave their homes and explore the different still mysterious regions of Pandora. When an old threat resurfaces, Jake will have to fight an uphill war against the humans. ',
+                        'img'          : 'https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/11/avatar-the-way-of-water-poster.jpg',
+                        'link'         : 'https://vido.lol/embed-i6do9a23wina.html?',
+                    },
+    
+
+};
+
+const container = document.querySelector('.container');
+
+for (const [key, value] of Object.entries(movies)) {
+
+        let div = document.createElement('div');
+        let h2  = document.createElement('h2');
+        let h3  = document.createElement('h3');
+        let p   = document.createElement('p');
+        let img = document.createElement('img');
+        let a   = document.createElement('a');
+        a.innerText = "WATCH";
+
+        div.setAttribute('class', 'container-section-movie');
+
+    for (const [k, v] of Object.entries(value)) {
+
+        h2.innerText = key;
+        h3.innerText = value['sous-title'];
+        p.innerText  = value['description'];
+        img.setAttribute('src', value['img'] );
+        a.setAttribute('href', value['link']);
+
+      }
+
+      div.append(h2);
+      div.append(h3);
+      div.append(p);
+      div.append(img);
+      div.append(a);
+      container.append(div);
+  }
+
 document.addEventListener('DOMContentLoaded', function() {
     const searchButton = document.querySelector('.search-button');
+    const searchInput = document.querySelector('.search-bar');
     const titles = document.querySelectorAll('h2');
     const noResultsMessage = document.querySelector('.no-results');
     const movieImages = document.querySelectorAll('.imgs');
 
-    searchButton.addEventListener('click', function(e) {
-        e.preventDefault();
-        const searchValue = document.querySelector('.search-bar').value.toLowerCase();
+    function performSearch() {
+        const searchValue = searchInput.value.toLowerCase();
         let resultsFound = false;
 
         titles.forEach(title => {
@@ -26,14 +178,5 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             noResultsMessage.style.display = 'block';
         }
-    });
-    movieImages.forEach(image => {
-        image.addEventListener('mouseenter', function() {
-            image.style.transform = 'scale(1.05)';
-        });
-
-        image.addEventListener('mouseleave', function() {
-            image.style.transform = 'scale(1)';
-        });
-    });
+    }
 });
